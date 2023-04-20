4218478c56cde654e1d2f37eea399355611ae1ed
oleksiiswift <oleksii.codingKey@gmail.com>
2023-04-20 18:47:01 +0300

    setLargeTitleDisplayMode

---
+++ UIViewController/UIViewController+LargeTitle.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+extension UIViewController {
+    
+    func setLargeTitleDisplayMode(_ largeTitleDisplayMode: UINavigationItem.LargeTitleDisplayMode) {
+        switch largeTitleDisplayMode {
+        case .automatic:
+              guard let navigationController = navigationController else { break }
+            if let index = navigationController.children.firstIndex(of: self) {
+                setLargeTitleDisplayMode(index == 0 ? .always : .never)
+            } else {
+                setLargeTitleDisplayMode(.always)
+            }
+        case .always, .never:
+            navigationItem.largeTitleDisplayMode = largeTitleDisplayMode
+            navigationController?.navigationBar.prefersLargeTitles = true
+        @unknown default:
+                assertionFailure("never")
+        }
+    }
+}
